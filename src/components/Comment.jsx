13aa403e3736db3b4599f22661e7ef3f3e2ab993
NMBxNIGHT <thiranat_kakanmee@cmu.ac.th>
2023-08-10 14:31:42 +0700

    so close

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -19,20 +19,21 @@ export const Comment = ({
         className="rounded-circle"
         style={{ objectFit: "cover" }}
       />
-      <div
-        className="rounded rounded-3 p-2"
-        style={{ backgroundColor: "#E5E7EB" }}
-      >
-        <span className="fw-semibold">{username}</span>
-        <br />
-        <span>{commentText}</span>
-
-        <div className="d-flex align-items-center gap-1">
-          <img src={showLike} width={20}></img>
-          <span className="text-muted">{showLikeNum}</span>
-        </div>
-      </div>
       <div>
+        <div
+          className="rounded rounded-3 p-2"
+          style={{ backgroundColor: "#E5E7EB" }}
+        >
+          <span className="fw-semibold">{username}</span>
+          <br />
+          <span>{commentText}</span>
+
+          <div className="d-flex align-items-center gap-1">
+            <img src={showLike} width={20}></img>
+            <span className="text-muted">{showLikeNum}</span>
+          </div>
+        </div>
+
         {replies.map((rep, i) => (
           <Reply
             key={i}
